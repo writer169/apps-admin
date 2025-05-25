685eda718e02b06768fd6cfd53fdf8cfea8361fb
writer169 <148839750+writer169@users.noreply.github.com>
2025-05-25 10:10:09 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { verifyToken } from '@/lib/auth'
+import { verifyToken } from './lib/auth'
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
